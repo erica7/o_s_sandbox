5c3d53ea8994e0dad67795bf7b9c6459abcd3705
Erica <ericaesnider@gmail.com>
2017-10-20 21:57:49 -0500

    playing with textInput props, methods, and state; making a mess

--- waves/App.js
+++ waves/App.js
@@ -2,26 +2,47 @@ import React from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 class Item extends React.Component {
+  state = {
+    value: '',
+  }
   render() {
     return (
       <View style={styles.item}>
         <Text style={styles.font, styles.parameter}>{this.props.parameter}</Text>
-        <TextInput style={styles.textInput} placeholder="test" />
-        <Text style={styles.unit}>{this.props.unit}</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={this.handleUpdate}
+          autoCapitalize="characters"
+          placeholder="test"
+        />
+        <Text style={styles.unit}>{this.props.unit} {this.state.value}</Text>
       </View>
     )
   }
 }
 
-// class MyAppText extends React.Component {
-//   render() {
-//     return (
-//       <Text style={styles.allText}>{this.props.children}</Text>
-//     )
-//   }
-// }
-
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      speed: '',
+      n: '',
+      d: '',
+      l: '',
+      q: '',
+    };
+  }
+  // state = {
+  // }
+  handleUpdate = (text) => {
+    this.setState((state) => {
+      return {
+        handleUpdateText: text,
+      }
+    })
+  }
+
   render() {
     let testValue = this.parameter;
     return (
@@ -34,10 +55,27 @@ export default class App extends React.Component {
         <Item parameter="Plunger Diameter" unit="in"/>
         <Item parameter="Stroke" unit="in"/>
         <Item parameter="Flowrate" unit="gpm"/>
+        <View style={styles.item}>
+          <Text style={styles.font, styles.parameter}>Erica {this.state.s}</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(text) => this.setState({ value: text })}
+            placeholder="Koba"
+          />
+          <Text style={styles.unit}>America {this.state.value}</Text>
+        </View>
       </View>
     );
   }
 }
+
+// class MyAppText extends React.Component {
+//   render() {
+//     return (
+//       <Text style={styles.allText}>{this.props.children}</Text>
+//     )
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -67,15 +105,16 @@ const styles = StyleSheet.create({
   // },
   parameter: {
     flex: 2,
-    backgroundColor: 'steelblue'
+    backgroundColor: '#bbb',
   },
   textInput: {
     flex: 2,
-    backgroundColor: 'skyblue'
+    color: '#111',
+    backgroundColor: '#ccc',
   },
   unit: {
     flex: 1,
-    backgroundColor: 'cornflowerblue'
+    backgroundColor: '#ddd',
   }
 
 });
